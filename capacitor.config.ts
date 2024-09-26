4f821ee10bcbad8b0bbc79bd35cb0027ea45af22
Rudy Casilla <94497674+rpccode@users.noreply.github.com>
2024-09-26 19:06:19 -0400

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'car.app',
+  appName: 'car-app',
+  webDir: 'www'
+};
+
+export default config;
